b1b87b3a73db915b6b1c27bf2d5b77123ed9c4a5
eoryazantseva <eoryazantseva@gmail.com>
2023-08-28 16:00:28 +1000

    added arrows for moving items up and down the list

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ function updateTodoList() {
         const newTodoText = document.createElement('p') // <p></p>
         newTodoText.innerHTML = todo // <p>Quit my Job</p>
 
+
+
+        const markImportanceUp = document.createElement('i') // <i></i>
+        markImportanceUp.type = 'button'
+        markImportanceUp.className = 'arrow up'
+        markImportanceUp.onclick = function() {
+            moveTaskUp(index)
+        }
+
+        const markImportanceDown = document.createElement('i') // <i></i>
+        markImportanceDown.type = 'button'
+        markImportanceDown.className = 'arrow down'
+        markImportanceDown.onclick = function() {
+            moveTaskDown(index)
+        }
+
+
         const markCompleteButton = document.createElement('input') //<input></input>
         markCompleteButton.type = 'button' // <input type="button"></input>
         markCompleteButton.onclick = function() {
@@ -26,9 +43,16 @@ function updateTodoList() {
         }
         markCompleteButton.value = 'Complete' // <input type="button" value="Complete" />
 
+        const controlTaskButtons = document.createElement('div')
+        controlTaskButtons.appendChild(markImportanceUp)
+        controlTaskButtons.appendChild(markImportanceDown)
+        controlTaskButtons.appendChild(markCompleteButton)
+
         const newTodo = document.createElement('li')
         newTodo.appendChild(newTodoText)
-        newTodo.appendChild(markCompleteButton)
+        newTodo.appendChild(controlTaskButtons)
+  
+ 
             /**
              * <li>
              *  <p>Quit My Job</p>
@@ -49,6 +73,44 @@ function updateTodoList() {
 function toggleComplete(index) {
     todoStatus[index] = true;
     updateTodoList()
+}
+
+function moveTaskUp(index) {
+    if (index > 0) {
+        //Swap the tasks in the todoList array
+        const tempTodo = todoList[index]; // we use a temporary variable tempTodo to store the task at the current index
+        todoList[index] = todoList[index - 1]; // we replace the task at the current index with the task at the previous index 
+        todoList[index - 1] = tempTodo; // Finally, we place the task from tempTodo into the previous index.
+
+        // Swap the task statuses in the todoStatus array
+
+        const tempStatus = todoStatus[index];
+        todoStatus[index] = todoStatus[index -1];
+        todoStatus[index - 1] = tempStatus;
+
+        //Update the todo list display
+        updateTodoList();
+
+    }
+}
+
+function moveTaskDown(index) {
+    if (index < todoList.length - 1) {
+        //Swap the tasks in the todoList array
+        const tempTodo = todoList[index]; 
+        todoList[index] = todoList[index + 1]; 
+        todoList[index + 1] = tempTodo; 
+
+        // Swap the task statuses in the todoStatus array
+
+        const tempStatus = todoStatus[index];
+        todoStatus[index] = todoStatus[index + 1];
+        todoStatus[index + 1] = tempStatus;
+
+        //Update the todo list display
+        updateTodoList();
+
+    }
 }
 
 updateTodoList()
